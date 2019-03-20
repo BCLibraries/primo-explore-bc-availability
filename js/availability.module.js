003a547c77b79fa2e998d686576eda2651756e35
Luke Gaudreau <gaudreal@bc.edu>
2019-03-20 09:28:48 -0400

    Rename module bc-availability

--- js/availability.module.js
+++ js/availability.module.js
@@ -1,9 +1,10 @@
 angular
-    .module('bc-browzine', [])
+    .module('bc-availability', [])
     .controller('prmSearchResultAvailabilityLineAfterController', function($scope) {
     window.browzine.primo.searchResult($scope);
   }) 
     .component('prmSearchResultAvailabilityLineAfter', {
     bindings: { parentCtrl: '<' },
-    controller: 'prmSearchResultAvailabilityLineAfterController'
+    controller: 'prmSearchResultAvailabilityLineAfterController',
+	template: '<hathi-trust-availability hideOnline="true"></hathi-trust-availability>'
   });
