cc1356da0063a829713c72c3d8543c43ec63ed07
Luke Gaudreau <luke.gaudreau@bc.edu>
2020-04-03 10:34:06 -0400

    Add support for Hathi Emergency Temporary Access Service

--- js/availability.module.js
+++ js/availability.module.js
@@ -6,5 +6,5 @@ angular
     .component('prmSearchResultAvailabilityLineAfter', {
     bindings: { parentCtrl: '<' },
     controller: 'prmSearchResultAvailabilityLineAfterController',
-	template: '<hathi-trust-availability hideOnline="true"></hathi-trust-availability>'
+	template: '<hathi-trust-availability ignore-copyright="true" hideOnline="false"></hathi-trust-availability><p class="avail-note">Login required. Click the Login Button in Hathitrust after following the link above.</p>'
   });
